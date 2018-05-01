c120606d8a8c1a26d16593b252f3423d077440d5
Sebastián Orantes Chang <ater7991@gmail.com>
2018-04-30 22:23:03 -0600

    diagram

---
+++ MTuring/MTuring/Header.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTuring
+{
+    class Header
+    {
+        public int numberMovs { get; set; }
+        public int currentState { get; set; }
+        public string currentChar { get; set; }
+        public int nextMove { get; set; } // 1 right & 0 left
+    }
+}
