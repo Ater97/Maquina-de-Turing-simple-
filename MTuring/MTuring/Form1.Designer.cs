f1c8a19b79250dd5c82e6fb28d91a11b5f913cc1
Sebastián Orantes Chang <ater7991@gmail.com>
2018-05-01 20:30:56 -0600

    step by step finished

--- MTuring/MTuring/Form1.Designer.cs
+++ MTuring/MTuring/Form1.Designer.cs
@@ -158,11 +158,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(302, 231);
+            this.button1.Location = new System.Drawing.Point(443, 146);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(117, 23);
             this.button1.TabIndex = 13;
-            this.button1.Text = "button1";
+            this.button1.Text = "Step by step";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
--- MTuring/MTuring/Form1.cs
+++ MTuring/MTuring/Form1.cs
@@ -19,11 +19,13 @@ namespace MTuring
         }
 
         String originalString = "";
-        
+        Manager M;
+        Header MainHeader;
         char[] Characters = new char[100];
+        int position = 0;
         private void Run_btn_Click(object sender, EventArgs e)
         {
-            Manager M = new Manager();
+            M = new Manager();
             originalString = textBox1.Text.Trim() + "E";
             textBox1.Enabled = false;
             label7.Text = "";
@@ -34,9 +36,9 @@ namespace MTuring
                 if (comboBox1.Text.ToString() != "")
                 {
                     comboBox1.Enabled = false;
-                    Header MainHeader = new Header();
+                    MainHeader = new Header();
                     MainHeader.State = 0;
-                    int position = 0;
+                    position = 0;
                     Characters = originalString.ToCharArray();
 
                     rtbTape.ResetText();
@@ -45,26 +47,21 @@ namespace MTuring
                     rtbTape.SelectionAlignment = HorizontalAlignment.Center;
                     rtbTape.DeselectAll();
                     HighlightSymbol(0); // Highlight first symbol
+                    Thread.Sleep(200);
 
-                     while (M.IsFinished())
+                    while (M.IsFinished())
                      {
-                        Characters = originalString.ToCharArray();
                         //Thread.Sleep(500);
+                        //Thread.SpinWait(500);
+                        Characters = originalString.ToCharArray();
                         MainHeader = M.GetMachine(comboBox1.Text.ToString(), Characters[position], MainHeader.State);
-
+                        ChangeCharacter(position, MainHeader.Char);
+                        HighlightSymbol(position);
                         if (MainHeader.WhereMove == 1)
-                        {
-                            ChangeCharacter(position, MainHeader.Char);
-                            HighlightSymbol(position);
-                            position++;
-                        }
+                            position++;                        
                         else
-                        {
-                            ChangeCharacter(position, MainHeader.Char);
-                            HighlightSymbol(position);
                             position--;
-                        }
-
+                        
                         label7.Text = MainHeader.State.ToString();
                         label6.Text = MainHeader.NumberMovs.ToString();
                      }
@@ -117,11 +114,83 @@ namespace MTuring
             rtbTape.DeselectAll();
         }
 
+        bool firsttime = true;
         private void button1_Click(object sender, EventArgs e)
         {
+            if (firsttime)
+            {
+                M = new Manager();
+                originalString = textBox1.Text.Trim() + "E";
+                textBox1.Enabled = false;
+                label7.Text = "";
+                label6.Text = "";
+                textBox2.Text = "";
+            }
             
+
+            if (originalString.Trim() != "")
+            {
+                if (comboBox1.Text.ToString() != "")
+                {
+                    if (firsttime)
+                    {
+                        comboBox1.Enabled = false;
+                        MainHeader = new Header();
+                        MainHeader.State = 0;
+                        position = 0;
+                        Characters = originalString.ToCharArray();
+
+                        rtbTape.ResetText();
+                        rtbTape.Text = originalString.Trim();
+                        rtbTape.SelectAll();
+                        rtbTape.SelectionAlignment = HorizontalAlignment.Center;
+                        rtbTape.DeselectAll();
+                        HighlightSymbol(0); // Highlight first symbol
+                    }
+
+                    if (M.IsFinished())
+                    {
+                        Characters = originalString.ToCharArray();
+                        MainHeader = M.GetMachine(comboBox1.Text.ToString(), Characters[position], MainHeader.State);
+                        ChangeCharacter(position, MainHeader.Char);
+                        HighlightSymbol(position);
+                        if (MainHeader.WhereMove == 1)
+                            position++;
+                        else
+                            position--;
+
+                        label7.Text = MainHeader.State.ToString();
+                        label6.Text = MainHeader.NumberMovs.ToString();
+                    }
+                }
+                else MessageBox.Show("Select the Turing Machine");
+            }
+            else MessageBox.Show("Insert Text");
+            if (firsttime)
+            {
+                textBox1.Enabled = true;
+                comboBox1.Enabled = true;
+                firsttime = false;
+            }
+
+            if (!M.IsFinished())
+            {
+                firsttime = true;
+                if (M.p.ERROR)
+                    textBox2.Text = "Fail!";
+                else if (!M.p.ERROR)
+                    textBox2.Text = "Succed!";
+            }
         }
 
+        /*int t = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HighlightSymbol(t++);
+        }*/
+
+
+        // button1.PerformClick();
         /*private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics l = e.Graphics;
